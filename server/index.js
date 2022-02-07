24746e8b800220f295677cf9b0dedab674bc5122
Christian J Barratt <cbarratt.dev@gmail.com>
2022-02-07 13:35:44 -0500

    cleanup

--- server/index.js
+++ server/index.js
@@ -21,9 +21,6 @@ app.use("/posts", postRoutes);
 // Setup mongodb database , will be hosted using their cloud/atlas version.
 // https://www.mongodb.com/cloud/atlas
 
-// connection string taken from mongodb cluster
-
-// Use mongoose to connect to database
 // Parser, Topology values set to true || false to fix depreciation warnings
 // referenced from https://mongoosejs.com/docs/5.x/docs/deprecations.html
 mongoose
@@ -32,9 +29,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() =>
-    //   listen for port connection, if true output running message
     app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`))
   )
   .catch((error) => console.log(error.message));
 
-// mongoose.set("useFindAndModify", false);
